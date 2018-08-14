963d502ad33b93f2e70af2130fba326ee9042f99
Sergey Taranov <cept73@yandex.ru>
2018-08-15 01:42:42 +0300

    Increase timeout in ajax

--- ajax.php
+++ ajax.php
@@ -14,7 +14,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-set_time_limit (4);
+set_time_limit (15);
 
 // Result by default
 $result = array ('address' => false);
